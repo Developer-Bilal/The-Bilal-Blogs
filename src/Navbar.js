a903affe83470c17a0e7bdf82f7a5fb318fa5981
Muhammad Bilal <bilalchanna67@gmail.com>
2024-06-03 06:34:30 +0500

    Create and useFetch components added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>The Bilal Blogs</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </nav>
-    );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>The Bilal Blogs</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
